86bafa15052073ba88018a7c371ad76656799b90
mayankkuthar <gabbar8055neel@gmail.com>
2021-09-08 22:51:26 +0530

    Updated Answer 26 of Assignment-2 Q26_changed

--- Assignment/03- Conditionals and Loops/Answer/src/com/answer/A2_Q26.java
+++ Assignment/03- Conditionals and Loops/Answer/src/com/answer/A2_Q26.java
@@ -2,6 +2,6 @@ package com.answer;
 //Question-26
 public class A2_Q26 {
     public static void main(String[] args) {
-        System.out.println("Yes, Kunal can go out in month of August");
+        System.out.println("Number of Days kunal can go out in month of August : "+(31/2));
     }
 }
