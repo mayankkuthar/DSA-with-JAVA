20ade9fe37307b94009a699e3e6feacb379ca234
AreebAbdulGhani <92576221+AreebAbdulGhani@users.noreply.github.com>
2021-10-20 22:31:18 +0530

    Update Q_5.java

--- Assignment/02- First Java Programs/Answer/src/com/answer/Q_5.java
+++ Assignment/02- First Java Programs/Answer/src/com/answer/Q_5.java
@@ -1,22 +1,24 @@
-package com.answer;
+package com.questions;
 
 import java.util.Scanner;
-//Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
-public class Q_5 {
-    public static void main(String[] args) {
-        Scanner input =new Scanner(System.in);
-        char choice;
-        System.out.println("Operations:-\n+, -, *, /\nEnter your choice: ");
-        choice = input.next().charAt(0);
-        System.out.println("Enter First Number :");
-        int num1 = input.nextInt();
-        System.out.println("Enter second number :");
-        int num2 = input.nextInt();
-        int result =0;
-        if(choice == '+') result = num1+num2;
-        else if(choice == '-') result = num1-num2;
-        else if(choice == '*') result = num1*num2;
-        else if(choice == '/') result = num1/num2;
-        System.out.println("Result: "+num1+" "+choice+" "+num2+" = "+result);
-    }
-}
+
+public class PracticeQuestion5 {
+  public static void main(String[] args) {
+
+      Scanner input = new Scanner(System.in);
+      char choice;
+      System.out.print("Operatiions:-\n+, -, *, /\nEnter your choice : ");
+      choice = input.next().charAt(0);
+      System.out.print("Enter First Number :");
+      int num1 = input.nextInt();
+      System.out.print("Enter Second number :");
+      int num2 = input.nextInt();
+      int result = 0;
+      if (choice == '+') result = num1+num2;
+      if (choice == '-') result = num1-num2;
+      if (choice == '*') result = num1*num2;
+      if (choice == '/') result = num1/num2;
+      System.out.println("Result: " + num1 + " " + choice + " " + num2 + " = " + result);
+
+  }
+  }
